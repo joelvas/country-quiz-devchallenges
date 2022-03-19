9f9c492dc0be4ee093905bd8612617acebc96597
jsonvas <ysonvas@gmail.com>
2022-03-18 23:20:41 -0500

    spanish added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="flex flex-row justify-center bg-indigo-600 items-center h-screen">
-    <QuizForm
-      :questionData="questionData"
-      @setResult="handleResult"
-      :points="points"
-      @tryAgain="tryAgain"
-    />
+  <div class="flex flex-col justify-center bg-indigo-600 h-screen">
+    <div class="position absolute top-0 flex flex-row ml-3">
+      <div
+        :class="[
+          'px-4 py-2 my-2 mx-1 text-[13px]',
+          lang === 'es' ? 'bg-slate-800 text-white' : 'bg-indigo-600',
+          'outline outline-1 outline-slate-800 rounded-md',
+          'hover:bg-slate-800 hover:text-slate-200',
+        ]"
+        @keyup="() => {}"
+        @click="setSpanish()"
+        style="cursor: pointer"
+      >
+        Español
+      </div>
+      <div
+        :class="[
+          'px-4 py-2 my-2 mx-1 text-[13px]',
+          lang === 'en' ? 'bg-slate-800 text-white' : 'bg-indigo-600',
+          'outline outline-1 outline-slate-800 rounded-md',
+          'hover:bg-slate-800 hover:text-slate-200',
+        ]"
+        @keyup="() => {}"
+        @click="setEnglish()"
+        style="cursor: pointer"
+      >
+        English
+      </div>
+    </div>
+    <div class="flex flex-row justify-center bg-indigo-600">
+      <QuizForm
+        :questionData="questionData"
+        @nextQuestion="nextQuestion"
+        :points="points"
+        @tryAgain="tryAgain"
+        :lang="lang"
+        :title="title"
+      />
+    </div>
   </div>
 </template>
 
@@ -21,14 +53,19 @@ export default {
     return {
       baseUrl: 'https://restcountries.com/v3.1/all',
       points: 0,
+      lang: 'en',
+      title: {
+        en: 'COUNTRY QUIZ',
+        es: 'QUIZ DE PAISES',
+      },
       questionData: {
         image: '',
-        question: '-----------------------------------?',
+        question: '....................loading',
         options: [
-          { text: '--------', checked: false },
-          { text: '--------', checked: false },
-          { text: '--------', checked: false },
-          { text: '--------', checked: false },
+          { text: '........', checked: false },
+          { text: '........', checked: false },
+          { text: '........', checked: false },
+          { text: '........', checked: false },
         ],
         correctOption: 'Syria',
       },
@@ -40,11 +77,21 @@ export default {
     document.title = 'DevChallenges | Country Quiz';
   },
   methods: {
-    handleResult(valid) {
+    setSpanish() {
+      this.lang = 'es';
+    },
+    setEnglish() {
+      this.lang = 'en';
+    },
+    nextQuestion(valid) {
       if (valid) {
         this.points += 1;
         this.randomQuestion();
       }
+    },
+    tryAgain() {
+      this.points = 0;
+      this.randomQuestion();
     },
     randomQuestion() {
       const elec = Math.round(Math.random() * 3);
@@ -60,52 +107,97 @@ export default {
       const res = await axios.get(this.baseUrl);
       if (res) {
         this.countries = res.data;
+        console.log(this.countries);
         this.randomQuestion();
       }
     },
-    tryAgain() {
-      this.points = 0;
-      this.randomQuestion();
-    },
     getQuestionAboutCountryCapital() {
+      const ctrs = [
+        this.countries[Math.round(Math.random() * 250)],
+        this.countries[Math.round(Math.random() * 250)],
+        this.countries[Math.round(Math.random() * 250)],
+      ];
+
       const options = [
-        { text: this.countries[Math.round(Math.random() * 250)].name.common, checked: false },
-        { text: this.countries[Math.round(Math.random() * 250)].name.common, checked: false },
-        { text: this.countries[Math.round(Math.random() * 250)].name.common, checked: false },
+        {
+          text: ctrs[0].name.common,
+          checked: false,
+          translatedText: ctrs[0].translations.spa.common,
+        },
+        {
+          text: ctrs[1].name.common,
+          checked: false,
+          translatedText: ctrs[1].translations.spa.common,
+        },
+        {
+          text: ctrs[2].name.common,
+          checked: false,
+          translatedText: ctrs[2].translations.spa.common,
+        },
       ];
 
       const country = this.countries[Math.round(Math.random() * 250)];
       const image = false;
       const correctOption = country.name.common;
-      options.push({ text: correctOption, checked: false });
+      options.push({
+        text: correctOption,
+        checked: false,
+        translatedText: country.translations.spa.common,
+      });
       options.sort(() => Math.random() - 0.5);
       const question = `${country.capital[0]} is the capital of?`;
+      const translatedQuestion = `${country.capital[0]} es la capital de?`;
 
       const data = {
         image,
         question,
+        translatedQuestion,
         options,
         correctOption,
       };
       this.questionData = data;
     },
     getQuestionAboutCountryFlag() {
+      const ctrs = [
+        this.countries[Math.round(Math.random() * 250)],
+        this.countries[Math.round(Math.random() * 250)],
+        this.countries[Math.round(Math.random() * 250)],
+      ];
+
       const options = [
-        { text: this.countries[Math.round(Math.random() * 250)].name.common, checked: false },
-        { text: this.countries[Math.round(Math.random() * 250)].name.common, checked: false },
-        { text: this.countries[Math.round(Math.random() * 250)].name.common, checked: false },
+        {
+          text: ctrs[0].name.common,
+          checked: false,
+          translatedText: ctrs[0].translations.spa.common,
+        },
+        {
+          text: ctrs[1].name.common,
+          checked: false,
+          translatedText: ctrs[1].translations.spa.common,
+        },
+        {
+          text: ctrs[2].name.common,
+          checked: false,
+          translatedText: ctrs[2].translations.spa.common,
+        },
       ];
 
       const country = this.countries[Math.round(Math.random() * 250)];
       const image = country.flags.png;
       const correctOption = country.name.common;
-      options.push({ text: correctOption, checked: false });
+      options.push({
+        text: correctOption,
+        checked: false,
+        translatedText: country.translations.spa.common,
+      });
       options.sort(() => Math.random() - 0.5);
       const question = 'Which country does this flag belong to?';
+      const translatedQuestion = 'A qué país pertenece esta bandera?';
 
       const data = {
         image,
         question,
+        translatedQuestion,
         options,
         correctOption,
       };
@@ -121,33 +213,53 @@ export default {
 
       let largestCountry = {
         area: 0,
-        country: '',
+        name: '',
+        translatedName: '',
       };
       ctrs.forEach((item) => {
         if (item.area > largestCountry.area) {
           largestCountry = {
             area: item.area,
-            country: item.name.common,
+            name: item.name.common,
+            translatedName: item.translations.spa.common,
           };
         }
       });
 
       const options = [
-        { text: ctrs[0].name.common, checked: false },
-        { text: ctrs[1].name.common, checked: false },
-        { text: ctrs[2].name.common, checked: false },
-        { text: ctrs[3].name.common, checked: false },
+        {
+          text: ctrs[0].name.common,
+          translatedText: ctrs[0].translations.spa.common,
+          checked: false,
+        },
+        {
+          text: ctrs[1].name.common,
+          translatedText: ctrs[1].translations.spa.common,
+          checked: false,
+        },
+        {
+          text: ctrs[2].name.common,
+          translatedText: ctrs[2].translations.spa.common,
+          checked: false,
+        },
+        {
+          text: ctrs[3].name.common,
+          translatedText: ctrs[3].translations.spa.common,
+          checked: false,
+        },
       ];
 
       const image = false;
-      const correctOption = largestCountry.country;
+      const correctOption = largestCountry.name;
       options.sort(() => Math.random() - 0.5);
-      const question = 'Which country is the largest?';
+      const question = 'Which country is the largest one?';
+      const translatedQuestion = 'Qué país es el mas grande?';
 
       const data = {
         image,
         question,
         options,
+        translatedQuestion,
         correctOption,
       };
       this.questionData = data;
--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="w-5/6 md:w-1/2 lg:w-2/6">
-    <div class="text-[20px] font-bold pb-2 text-white">COUNTRY QUIZ</div>
+    <div class="text-[20px] font-bold pb-2 text-white" v-if="lang === 'en'">{{ title.en }}</div>
+    <div class="text-[20px] font-bold pb-2 text-white" v-if="lang === 'es'">{{ title.es }}</div>
     <div class="bg-white rounded-md flex flex-col items-center justify-center h-96" v-if="results">
-      <div class="text-[40px] font-bold text-slate-800">Results</div>
-      <div class="flex flex-row items-center">
+      <div class="text-[40px] font-bold text-slate-800" v-if="lang === 'en'">Results</div>
+      <div class="text-[40px] font-bold text-slate-800" v-if="lang === 'es'">Resultados</div>
+      <div class="flex flex-row items-center" v-if="lang === 'en'">
         You got
         <div class="px-2 text-[24px] font-bold text-green-600">{{ points }}</div>
         correct answers
+      </div>
+      <div class="flex flex-row items-center" v-if="lang === 'es'">
+        Tuviste
+        <div class="px-2 text-[24px] font-bold text-green-600">{{ points }}</div>
+        respuestas correctas
       </div>
       <div class="mt-12">
         <button
           class="outline outline-2 outline-slate-800 rounded-md py-2 px-8 text-[13px] font-bold"
           @click="tryAgain"
+          v-if="lang === 'es'"
+        >
+          Intentar de nuevo
+        </button>
+        <button
+          class="outline outline-2 outline-slate-800 rounded-md py-2 px-8 text-[13px] font-bold"
+          @click="tryAgain"
+          v-if="lang === 'en'"
         >
           Try Again
         </button>
@@ -21,7 +36,8 @@
       <div v-if="typeof question.image === 'string'">
         <img :src="question.image" class="w-20" alt="flag" />
       </div>
-      <div class="font-extrabold mt-4">{{ question.question }}</div>
+      <div class="font-extrabold mt-4" v-if="lang === 'es'">{{ question.translatedQuestion }}</div>
+      <div class="font-extrabold mt-4" v-if="lang === 'en'">{{ question.question }}</div>
       <div class="pt-4">
         <div
           style="cursor: pointer"
@@ -60,13 +76,24 @@
               'group-hover:text-white',
             ]"
           >
-            {{ item.text }}
+            {{ lang === 'en' ? item.text : item.translatedText }}
           </div>
         </div>
       </div>
       <div class="flex flex-row justify-end" v-if="answered">
-        <button class="bg-yellow-500 px-6 py-2 rounded-md text-white" @click="handleResult">
+        <button
+          class="bg-yellow-500 px-6 py-2 rounded-md text-white"
+          @click="nextQuestion"
+          v-if="lang === 'en'"
+        >
           Next
+        </button>
+        <button
+          class="bg-yellow-500 px-6 py-2 rounded-md text-white"
+          @click="nextQuestion"
+          v-if="lang === 'es'"
+        >
+          Siguiente
         </button>
       </div>
     </div>
@@ -86,6 +113,18 @@ export default {
       type: Number,
       default() {
         return 0;
+      },
+    },
+    lang: {
+      type: String,
+      default() {
+        return '';
+      },
+    },
+    title: {
+      type: Object,
+      default() {
+        return {};
       },
     },
   },
@@ -119,11 +158,12 @@ export default {
         this.question.options[i].checked = true;
       }
     },
-    handleResult() {
+    nextQuestion() {
       if (this.valid) {
-        this.$emit('setResult', this.valid);
+        this.$emit('nextQuestion', this.valid);
         this.answered = false;
         this.valid = false;
+        this.results = false;
       } else {
         this.results = true;
       }
